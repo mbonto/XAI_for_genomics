840d10a141013378a487b405887664347b7ae74c
mbonto <45796859+mbonto@users.noreply.github.com>
2023-03-21 18:47:40 +0100

    Update get_pancan.py

--- Pancan/Data/get_pancan.py
+++ Pancan/Data/get_pancan.py
@@ -3,12 +3,12 @@ import os
 import sys
 code_path = os.path.split(os.path.split(os.getcwd())[0])[0]
 sys.path.append(code_path)
-from setting import data_path
+from setting import *
 from download_data import *
 
 
 # Download
-data_path = os.path.join(data_path, 'tcga')
+data_path = get_data_path('pancan')
 database = 'pancan'
 cancer = 'pancan'
 download_dataset(data_path, database, cancer)
